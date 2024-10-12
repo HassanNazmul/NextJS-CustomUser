594c2e2c51f8a950417974b6a6580c097f1d0463
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2024-10-12 22:31:28 +0100

    Initial Commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -34,7 +34,7 @@ const Dashboard: React.FC = () => {
             <button onClick={handleLogout} className="mt-4 px-4 py-2 bg-red-500 text-white rounded">
                 Logout
             </button>
-            <AdminInfo/>
+            {/*<AdminInfo/>*/}
         </div>
     );
 };
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,10 @@
-"use client";
-
 import React from 'react';
 import Login from "@/components/login/Login";
 
 function LoginPage() {
     return (
         <main>
-            <Login onSubmit={(email, password) => {
-                console.log("Form submitted:", email, password);
-            }}/>
+            <Login/>
         </main>
     );
 }
